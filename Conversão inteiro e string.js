7b7789673d92b2f62122f2ea9a9d53b4f732a376
Wanderson Alves Vieira <wandersonav07@gmail.com>
2023-03-04 08:32:27 -0300

    JavaScriptpBack

---
+++ Conversão inteiro e string.js
@@ -0,0 +1,13 @@
+// String();
+// Number();
+// Convertendo string em number e vice versa.
+
+
+let numero = 10;
+let string = '11';
+let soma;
+
+
+console.log(numero + Number(string));
+
+
---
+++ truthy ou falsy.js
@@ -0,0 +1,18 @@
+let variavel = 2;
+let teste = 3;
+let total;
+let variavel1 = null;
+
+if (variavel1 == null){
+
+    
+
+}else{
+
+
+
+
+}
+
+
+console.log(typeof total);
